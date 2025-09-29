1237bbfebd0d44dc8e7874a72e5b6bd21f2cf9ef
Emanuel Fortes <emanuelbf19@gmail.com>
2025-09-29 18:36:05 -0300

    home
    Metade da pagina home sendo finalizada

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+
+var swiper1 = new Swiper(".mySwiper1", {
+  direction: "vertical",
+  slidesPerView: 1,
+  spaceBetween: 30,
+  autoplay: true,
+  mousewheel: false,
+  loop: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  speed: 5000, // quanto maior, mais lento o movimento
+  autoplay: {
+    delay: 0, // contínuo
+    disableOnInteraction: false,
+  },
+  allowTouchMove: false
+});
