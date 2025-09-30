c2294d392196d0a5f5bbe7e0fc9ab50554ebebf8
Emanuel Fortes <emanuelbf19@gmail.com>
2025-09-29 22:30:59 -0300

    removendo foto desnecessaria

--- src/js/script.js
+++ src/js/script.js
@@ -6,15 +6,3 @@ var swiper1 = new Swiper(".mySwiper1", {
   mousewheel: false,
   loop: true,
 });
-// var swiper2 = new Swiper(".mySwiper2", {
-//   slidesPerView: 4,
-//   spaceBetween: 30,
-//   centeredSlides: true,
-//   loop: true,
-//   speed: 5000, // quanto maior, mais lento o movimento
-//   autoplay: {
-//     delay: 0, // contínuo
-//     disableOnInteraction: false,
-//   },
-//   allowTouchMove: false
-// });
