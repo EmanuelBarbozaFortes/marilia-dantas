3337c8b519b1e9fed1a6abb8825a7a7be9928bd2
Emanuel Fortes <emanuelbf19@gmail.com>
2025-09-30 19:43:12 -0300

    subindo galeria de serviços marilia

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,22 @@ var swiper1 = new Swiper(".mySwiper1", {
   mousewheel: false,
   loop: true,
 });
+var swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 30,
+  breakpoints: {
+    320: { // mobile
+      slidesPerView: 1,
+      grid: { rows: 3 },
+    },
+    640: { // tablets
+      slidesPerView: 2,
+      grid: { rows: 2 },
+    },
+    1024: { // desktops
+      slidesPerView: 3,
+      grid: { rows: 2 },
+    },
+  },
+  loop: true,
+
+});
